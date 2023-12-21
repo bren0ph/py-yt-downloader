a9f2ab4c9de15066ab09cd28eed05cef83e284d1
Breno P. Henrique <brenoph.432@hotmail.com>
2023-12-21 20:04:50 -0300

    Adaptação para linha de comando (CLI)

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class Baixador(Thread):
         self.autor = u'%s' % (autor)
         Thread.__init__(self)
 
+    # executa o processo de download
     def run(self):
         try:
             # baixa o vídeo
@@ -23,6 +24,8 @@ class Baixador(Thread):
             # converte para mp3
             tempVideo = os.path.join(self.saida,"%s.mp4" % (self.link.title))
             saidaMp3 = os.path.join(self.saida,f"%s.mp3" % (self.link.title))
+
+        # caso não consiga salvar com o título do vídeo, salva com o nome informado
         except:
             self.stream.download(output_path=self.saida,filename=f'{self.nome}.mp4')
 
@@ -40,20 +43,68 @@ class Baixador(Thread):
         audio.tag.save()
         os.remove(tempVideo)
 
-# abre o arquivo com os links
-videos = open('links.txt','r',encoding="utf-8")
+# modificações para branch da versão de linha de comando (CLI)
+videos = []
 threads = []
 
-for video in videos:
-    print(video)
-    dados = video.strip().split(sep='\t') # o \t é o TAB
-    link = dados[0]
-    nome = u'%s' % (dados[1])
-    autor = u'%s' % (dados[2])
-    threads.append(Baixador(link,nome,autor))
+# prepara as threads para os downloads
+def baixa():
+    for video in videos:
+        link = video['link']
+        nome = video['nome']
+        autor = video['autor']
+        threads.append(Baixador(link,nome,autor))
 
-for thread in threads:
-    thread.start()
+    for thread in threads:
+        thread.start()
 
-for thread in threads:
-    thread.join()
+    for thread in threads:
+        thread.join()
+
+# coloca um vídeo na lista de download
+def adiciona():
+    link = input("Link: ")
+    nome = input("Nome: ")
+    autor = input("Autor: ")
+
+    video = dict(link=link,nome=nome,autor=autor)
+
+    videos.append(video)
+
+# mostra quais vídeos estão prontos para download
+def lista():
+    print("Indice - Link - Nome - Autor")
+    i = 0
+    for video in videos:
+        print(" - ".join([str(i),video['link'],video['nome'],video['autor']]))
+        i += 1
+
+# remove um vídeo da lista
+def apaga():
+    ind = input("Índice: ")
+    del videos[ind]
+
+# menu principal (o coração da branch)
+def menu():
+    op = 0
+    while op < 5:
+        print("MENU:")
+        print("1 - Adicionar músicas para download")
+        print("2 - Listar músicas para download")
+        print("3 - Remover música para download")
+        print("4 - Iniciar download")
+        print("5+ - Sair")
+        op = int(input("> "))
+
+        if op == 1:
+            adiciona()
+        elif op == 2:
+            lista()
+        elif op == 3:
+            apaga()
+        elif op == 4:
+            baixa()
+        else:
+            break
+
+menu()
